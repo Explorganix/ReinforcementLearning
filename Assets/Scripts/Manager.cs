4a6a0d32cf6eb14895aee18e342e3a657bba20b8
LoveTrev <tlove@spsu.edu>
2016-04-30 14:52:03 -0400

    first commit
    first

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Manager : MonoBehaviour {
+
+    public GameObject tile;
+    public int worldWidth;
+    public int worldHeight;
+    public float tileSize;
+    public int goldX;
+    public int goldY;
+
+    public List<int> wallXCoords;
+    public List<int> wallYCoords;
+
+
+
+    public Vector2 currentPos;
+
+    [SerializeField]
+    GameObject[,] gridWorld;
+	// Use this for initialization
+	void Start () {
+        gridWorld = new GameObject[worldHeight, worldWidth];
+        tileSize = tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        GenerateGridWorld();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GenerateGridWorld()
+    {
+        for(int i = 0; i < worldHeight; i++)
+        {
+            for(int j = 0; j < worldWidth; j++)
+            {
+                gridWorld[j,i] = (GameObject)Instantiate(tile, new Vector3(tileSize * j, tileSize * i, 0), Quaternion.identity);
+                gridWorld[j, i].GetComponent<Tile>().x = j;
+                gridWorld[j, i].GetComponent<Tile>().y = i;
+
+                if(j == goldX)
+                {
+                    if(i == goldY)
+                    {
+                        gridWorld[j, i].GetComponent<Tile>().SetColor(Color.yellow);
+                        gridWorld[j, i].GetComponent<Tile>().SetReward(1);
+                    }
+                }
+
+               
+              
+                Debug.Log("Tile [" + j + "," + i + "] color is :" + gridWorld[j,i].GetComponent<Tile>().GetColor().ToString());
+                Debug.Log("Reward : " + gridWorld[j, i].GetComponent<Tile>().reward);
+            }
+        }
+        for (int k = 0; k < wallXCoords.Count; k++)
+        {
+            int x = wallXCoords[k];
+            int y = wallYCoords[k];
+            gridWorld[x, y].GetComponent<Tile>().SetColor(Color.black);
+        }
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+    public SpriteRenderer sprite;
+    public Color color;
+    public int x;
+    public int y;
+    public int reward;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public Color GetColor()
+    {
+        return color;
+    }
+
+    public void SetColor(Color col)
+    {
+        color = col;
+        sprite.material.color = col;
+    }
+
+    public int GetReward()
+    {
+        return reward;
+    }
+
+    public void SetReward(int rew)
+    {
+        reward = rew;
+    }
+}
