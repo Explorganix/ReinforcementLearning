d3adef39849f3b9e13aca4c07df398ca9f3a4121
LoveTrev <tlove@spsu.edu>
2016-05-01 13:30:35 -0400

    player position generating randomly
    player position is spawning randomly

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class Manager : MonoBehaviour {
 
@@ -12,6 +13,7 @@ public class Manager : MonoBehaviour {
 
     public List<int> wallXCoords;
     public List<int> wallYCoords;
+    public List<Vector2> wallAndGoldPositions;
 
 
 
@@ -24,10 +26,13 @@ public class Manager : MonoBehaviour {
         gridWorld = new GameObject[worldHeight, worldWidth];
         tileSize = tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
         GenerateGridWorld();
+        currentPos = SetStartPosition(worldHeight, worldWidth);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 
@@ -41,26 +46,48 @@ public class Manager : MonoBehaviour {
                 gridWorld[j, i].GetComponent<Tile>().x = j;
                 gridWorld[j, i].GetComponent<Tile>().y = i;
 
+                
                 if(j == goldX)
                 {
                     if(i == goldY)
                     {
-                        gridWorld[j, i].GetComponent<Tile>().SetColor(Color.yellow);
-                        gridWorld[j, i].GetComponent<Tile>().SetReward(1);
+                        //gridWorld[j, i].GetComponent<Tile>().SetColor(Color.yellow);
+                       // gridWorld[j, i].GetComponent<Tile>().SetReward(1);
+                       // wallAndGoldPositions.Add(new Vector2(j, i));
                     }
-                }
-
-               
-              
-                Debug.Log("Tile [" + j + "," + i + "] color is :" + gridWorld[j,i].GetComponent<Tile>().GetColor().ToString());
-                Debug.Log("Reward : " + gridWorld[j, i].GetComponent<Tile>().reward);
+                }  
+                //Debug.Log("Tile [" + j + "," + i + "] color is :" + gridWorld[j,i].GetComponent<Tile>().GetColor().ToString());
+                //Debug.Log("Reward : " + gridWorld[j, i].GetComponent<Tile>().reward);
             }
         }
+
+        //set gold position
+        gridWorld[goldX, goldY].GetComponent<Tile>().SetColor(Color.yellow);
+        gridWorld[goldX, goldY].GetComponent<Tile>().SetReward(1);
+        wallAndGoldPositions.Add(new Vector2(goldX, goldY));
+
+        //set tile color and reward for walls
         for (int k = 0; k < wallXCoords.Count; k++)
         {
             int x = wallXCoords[k];
             int y = wallYCoords[k];
             gridWorld[x, y].GetComponent<Tile>().SetColor(Color.black);
+            gridWorld[x, y].GetComponent<Tile>().SetReward(-1);
+            wallAndGoldPositions.Add(new Vector2(x,y));
+        }
+    }
+
+    private Vector2 SetStartPosition(int worldHeight, int worldWidth)
+    {
+        Vector2 pos = new Vector2(UnityEngine.Random.Range(0, worldHeight), UnityEngine.Random.Range(0, worldWidth));
+        if (gridWorld[(int)pos.x, (int)pos.y].GetComponent<Tile>().reward == 0)
+        {
+            gridWorld[(int)pos.x, (int)pos.y].GetComponent<Tile>().SetColor(Color.green);
+            return pos;
+        }
+        else
+        {
+            return SetStartPosition(worldHeight, worldWidth);
         }
     }
 }
